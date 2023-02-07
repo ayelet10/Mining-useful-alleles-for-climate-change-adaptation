749f2cc639a08cdeff7e30263ddc2c4b7f8e6ace
Ayelet Salman-Minkov <ayelet.salman@gmail.com>
2023-02-07 15:20:43 -0800

    Update prepare_files_for_genomicSimulation.R
    Rscript to prepare files for the breeding simulation with genomicSimulation R package. 
    Outputs: genetic map file, mutations effect sizes file, tester genotype file as txt

--- prepare_files_for_genomicSimulation.R
+++ prepare_files_for_genomicSimulation.R
@@ -4,7 +4,6 @@
 args <- commandArgs(trailingOnly = TRUE) # TRUE
 print(args)
 seed_num =args[1]  
-# seed_num =1231227  #1231214 ##  #
 my_wd <- sprintf("/home/salmanay/projects/breeding_simulations/%s/",seed_num )
 
 dir.create(file.path(my_wd)) # if dir exists, it just gives a warning
@@ -22,81 +21,34 @@ ChangeDuplicatedPositions <- function(markers_mat) {
   unique_dupicated_sites <- unique(duplicated_loci$pos)
   for(u in unique_dupicated_sites){
     curr_dupl_rows <- duplicated_loci[duplicated_loci$pos %in% u, ]
-    # jump_between_dupl <- 1/(nrow(curr_dupl_rows)+ 1) ## added 1 to account for the first occurence # the jump changes according to the number of dupicants on the same position
     jump_between_dupl <- 0.00000001
     steps_to_add <- seq(jump_between_dupl,jump_between_dupl*nrow(curr_dupl_rows), by = jump_between_dupl)
     curr_dupl_rows$pos <- as.numeric(curr_dupl_rows$pos) + steps_to_add 
-    
-    markers_mat[rownames(markers_mat) %in% rownames(curr_dupl_rows),] <- curr_dupl_rows # replace duplicated rows with corrected positions rows
+   # replace duplicated rows with corrected positions rows
+    markers_mat[rownames(markers_mat) %in% rownames(curr_dupl_rows),] <- curr_dupl_rows 
   }
   
   return(markers_mat)
 }
-##############################
-## prepare genomic map file ## >> read the vcf and add unique chromosome number to every 50kb block. File header need to be: marker	chr	pos 
-###################################################
-# marker - the snp name                           #
-# pos - the position on the "chromosome"          #
-# chr - 1-20 linkage groups as chromosomes 1-20   #
-###################################################
-# full_vcf_file <- sprintf("/home/salmanay/projects/Lotterhos_preprint_data/for_JeffRI/%s_plusneut_MAF01.recode2.vcf.gz", seed_num) # the tester is not dependent on the seed 
-# full_vcf <- read.vcfR(full_vcf_file, verbose = FALSE )
-## copy the first 3 columns to file for a genetic map creation  >> to try when farm is available
-# vcf-subset -c name1,name2,name3 /home/salmanay/projects/Lotterhos_preprint_data/for_JeffRI/1231227_plusneut_MAF01.recode2.vcf.gz | tr "\t" "," > first_three_columns_vcf_out.csv 
-
-
-# number of rows in vcf. 6 first rows are the header ### marker names are not read completly
-# zcat /home/salmanay/projects/Lotterhos_preprint_data/for_JeffRI/1231227_plusneut_MAF01.recode2.vcf.gz | wc -l
-
-# my_map <- read.csv(sprintf("%s.first_two_columns_recode_vcf_out.csv", seed_num) , skip = 5)
-# my_map <- read.csv(sprintf("%s.first_two_columns_recode_vcf_out_15Jan.csv", seed_num) , skip = 5, sep = "\t")
-############## 
-# input.file_vcf <- sprintf("/home/salmanay/projects/Lotterhos_preprint_data/for_JeffRI/%s_plusneut_MAF01.recode2.vcf.gz", seed_num) # non variable sites error ?
-# input.file_vcf <- sprintf("/home/salmanay/projects/breeding_simulations/%s/%s_hundred_over_hundred_recode_vcf_out_mac.vcf.recode.vcf" , seed_num,seed_num)   # 76 sites kept after vcftools -mac 1 for non variable sites removal
-
-# vcf <- read.vcfR(input.file_vcf, verbose = FALSE )
-# my_genotypes <- extract.gt(vcf)
-# print(head(my_genotypes[1:10,1:10]))
-# print("row names:")
-# print(rownames(my_genotypes[1:10,1:10]))
-# marker_names <- rownames(my_genotypes)
-
 
 ###############################
 ## prepare effect sizes file ##
 ###############################
 # output format: ## marker, allele, effect size ##
-# # mutations_file <- sprintf("/home/salmanay/projects/Lotterhos_preprint_data/for_JeffRI/%s_muts.txt", seed_num)
 mutations_file <- sprintf("/home/salmanay/projects/Lotterhos_preprint_data/for_JeffRI/%s_Rout_muts_full.txt", seed_num)
-# mutations_file <- sprintf("C:/Users/ayelet/Documents/UCDavis/scripts/simulations/genomicSimulation_tests/%s_Rout_muts_full_300_rows.txt", seed_num)
-# # mutations_file <- "C:/Users/ayelet/Documents/UCDavis/scripts/simulations/data_from_Lotterhos_preprint_2022/1231227_muts.txt"
-
 seed_mutations <- read.csv(mutations_file, sep = " ")
-# write.table( seed_mutations[1:300,], file = sprintf("%s_Rout_muts_full_300_rows.txt", seed_num), sep = " ")
-# print(head(seed_mutations))
-# print(unique(seed_mutations["mutname"]))
-
 my_columns <- c("mutname", "mutSalEffect" )
 with_mut <- seed_mutations[,my_columns]
-
 with_mut <- cbind(with_mut , rep(1, nrow(with_mut))) # the allele 1
 # # add rows for allele 0, (without the mutation), effect size set to 0
-# without_mut <- cbind(seed_mutations[c("mutname" )] , rep(0, nrow(with_mut)),rep(0, nrow(with_mut)) )
 with_mut[with_mut$mutSalEffect %in% NA,]$mutSalEffect <- 0
 # add positions column # save only positions numbers from marker names#
-print(head(with_mut))
 with_mut$pos <- sub(".*-", "" , with_mut$mutname)
 with_mut$pos <- sub("_.*" , "" ,with_mut$pos)
 colnames(with_mut) <- c(my_columns, "allele", "pos")
 with_mut[,"mutname"] <- gsub(".*-", "", with_mut[,"mutname"]) # update marker names format, remove "-" and digit before it 
 print(length(with_mut$pos))
 print(length(unique(with_mut$pos)))
-
-
-# print(with_mut[!with_mut$mutname %in% 1,])
-# colnames(without_mut) <- c(my_columns, "allele")
-# all_muts <- rbind(with_mut, without_mut, deparse.level = 0  )
-# write.table(with_mut[c("pos","allele","mutSalEffect")], file = sprintf("%s%s_effect_file_sal.txt",my_wd,seed_num ) , sep = " ", quote = FALSE,  col.names = FALSE, row.names = FALSE)
 
 write.table(with_mut[c("mutname","allele","mutSalEffect")], file = sprintf("%s%s_effect_file_sal.txt",my_wd,seed_num ) , sep = " ", quote = FALSE,  col.names = FALSE, row.names = FALSE)
 ###############################################################################
@@ -163,43 +115,3 @@ for (chr in 1:num_linkage_groups){
   my_map[my_map$chr %in% chr,] <- ChangeDuplicatedPositions(curr_chr)
 }
 write.table(my_map[,c("marker", "chr","pos" )], file= sprintf("%s_map_file_muts_full.txt", seed_num) , sep = "\t", quote = FALSE, row.names = FALSE)
-#############################
-## work with previous files #
-#############################
-# cnames <-  "allele"
-# ## the mutations with effect are listed in %s_muts.txt file, all the other have 0 effect. ## not needed if using _Rout_muts_full. file (?)
-# # all_markers_one <- matrix(rep("1" ,number_sites), ncol = 1, nrow = number_sites, dimnames=list(rnames))
-# all_markers_one <- matrix(rep("1" ,number_sites), ncol = 1, nrow = number_sites, dimnames=list(rnames)) # alleles
-# all_markers_one <- cbind(all_markers_one, my_map$marker)
-# all_markers_one <- cbind(all_markers_one, 0) ## effects
-# all_markers_one[all_markers_one[,2]%in% with_mut$mutID, 3] <- with_mut$mutSalEffect # in case there is a snp with effect size it will be assigned the effect size from the sim
-# ##
-# all_markers_zero <- matrix(rep("0" ,number_sites), ncol = 1, nrow = number_sites, dimnames=list(rnames))
-# all_markers_zero <- cbind(all_markers_zero, my_map$marker)
-# all_markers_zero <- cbind(all_markers_zero, 0) ## effects
-# 
-# all_markers <- rbind(all_markers_one,all_markers_zero )
-# head(all_markers)
-# write.table(all_markers[,c(2,1,3)], file = sprintf("%s%s_effect_file_all_markers.txt", my_wd, seed_num) , sep = " ", quote = FALSE,  col.names = FALSE, row.names = FALSE)
-# 
-############## general tester - not sure genomicSimulation can use it ###############
-# number_sites <-  num_linkage_groups*chr_size #10 # to take from the full_vcf
-# rnames <-  c(1:number_sites) #  c(rep(1:10))# genes names from full vcf file pos # 
-# cnames <-  "tester"
-# tester_geno <- matrix(rep("00" ,number_sites), ncol = 1, nrow = number_sites, dimnames=list(rnames,cnames))
-# tester_geno <- cbind(tester_geno, rnames)
-# head(tester_geno)
-# tester_geno <- tester_geno[,c(2,1)]
-# colnames(tester_geno) <- c("name",cnames )
-# write.table(tester_geno, file = sprintf("tester_allele_file.txt", seed_num), sep = "\t", quote = FALSE, row.names = FALSE)
-
-# ## check consistency of mutations between files ##
-# eff_file <- read.table(sprintf("%s_effect_file.txt", seed_num ),sep = " " )
-# eff_file <- read.table(sprintf("%s_effect_file_muts_only.txt", seed_num ),sep = " " )
-# map_file <-read.table(sprintf("%s_map_file.txt", seed_num),sep = "\t" , header = TRUE)
-# alleles <- read.table(sprintf("%s%s_updated_markers_names_allele_file_as_txt.txt",my_wd, pred_lr ),sep = "\t" , header = TRUE, colClasses=c('character','character'))
-# 
-# alleles[alleles$name %in% map_file$pos,] #
-# eff_file[eff_file$V1 %in% map_file$pos,] # 0
-# map_file[as.numeric(map_file$pos) %in% as.numeric(eff_file$V1) ,] # 0
-# alleles[alleles$name %in% eff_file$V1,]
